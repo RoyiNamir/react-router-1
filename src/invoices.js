7922444603d4da06a2c1f09fbb1e409ffe2413fc
Royi <>
2022-07-02 14:34:10 +0300

    d

---
+++ src/invoices.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { getInvoices } from "./data";
+import { Link , Outlet } from "react-router-dom";
+
+function Invoices() {
+  let invoices = getInvoices();
+
+  return (
+    <div style={{ display: "flex" }}>
+      <nav
+        style={{
+          borderRight: "solid 1px",
+          padding: "1rem",
+        }}
+      >
+        {invoices?.map((invoice) => (
+          <Link
+            style={{ display: "block", margin: "1rem 0" }}
+            to={`/invoices/${invoice.number}`}
+            key={invoice.number}
+          >
+            {invoice.name}
+          </Link>
+        ))}
+      </nav>
+      <Outlet />
+    </div>
+  );
+}
+
+export default Invoices;
